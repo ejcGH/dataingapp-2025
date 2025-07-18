84696bb743cd857abc858b5c203f84dfe25d06bf
ejcgh <elouisa.cardano@enterprisewireless.org>
2025-07-18 16:30:58 -0400

    first commit

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+//class we are deriving from, using a primary constructor
+public class AppDbContext(DbContextOptions options) : DbContext(options) 
+{          // this will set name of table will be "Users" 
+    public DbSet<AppUser> Users { get; set; }
+}
