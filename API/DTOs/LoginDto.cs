24ca8696e64f2aaeecf5f500890c8b1c8ec9066c
ejcGH <iam3jc@gmail.com>
2025-08-19 15:29:22 -0400

    Creating the token service

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace API;
+
+public class LoginDto
+{
+
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using API.Entities;
+
+namespace API.Serivces;
+
+public class TokenService : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+}
